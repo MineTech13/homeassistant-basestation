24e7a082b1b43f21d22a9db38a341ee2275e880b
Nils Z. <kiti.nz.kit@gmail.com>
2025-03-20 04:20:44 +0000

    Trying something no clue if it works thoo

--- custom_components/basestation/switch.py
+++ custom_components/basestation/switch.py
@@ -24,7 +24,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
 
 class BasestationSwitch(SwitchEntity):
-    """The basestation switch implementtion."""
+    """The basestation switch implementation."""
 
     def __init__(self, hass, mac, name):
         """Initialize the switch."""
@@ -45,7 +45,7 @@ class BasestationSwitch(SwitchEntity):
 
     @property
     def available(self):
-        """Return the connection status of this switch"""
+        """Return the connection status of this switch."""
         return self._is_available
 
     @property
@@ -53,50 +53,25 @@ class BasestationSwitch(SwitchEntity):
         """If the switch is currently on or off."""
         return self._is_on
 
-    def turn_on(self, **kwargs):
+    async def async_turn_on(self, **kwargs):
         """Turn the switch on."""
-        asyncio.run(self.async_turn_on())
+        await self._send_command(PWR_ON)
 
-    def turn_off(self, **kwargs):
+    async def async_turn_off(self, **kwargs):
         """Turn the switch off."""
-        asyncio.run(self.async_turn_off())
+        await self._send_command(PWR_STANDBY)
 
     @property
     def name(self):
         """Return the name of the switch."""
-        if self._name is not None:
-            return self._name
-
-        return "Valve Basestation"
-
-    def update(self):
-        """Fetch new state data for the sensor."""
-        asyncio.run(self.async_update())
-
-    async def async_turn_on(self):
-        try:
-            async with BleakClient(self.get_ble_device(), timeout=30) as client:
-                await client.write_gatt_char(PWR_CHARACTERISTIC, PWR_ON)
-        except asyncio.exceptions.TimeoutError:
-            _LOGGER.debug(
-                "Timeout occurred when trying to turn on basestation '%s'.",
-                self._mac,
-            )
-
-    async def async_turn_off(self):
-        try:
-            async with BleakClient(self.get_ble_device(), timeout=30) as client:
-                await client.write_gatt_char(PWR_CHARACTERISTIC, PWR_STANDBY)
-        except asyncio.exceptions.TimeoutError:
-            _LOGGER.debug(
-                "Timeout occurred when trying to turn off basestation '%s'.",
-                self._mac,
-            )
+        return self._name if self._name else "Valve Basestation"
 
     async def async_update(self):
+        """Fetch new state data for the sensor."""
         try:
             async with BleakClient(self.get_ble_device(), timeout=30) as client:
                 self._is_on = await client.read_gatt_char(PWR_CHARACTERISTIC) != PWR_STANDBY
+                self._is_available = True
         except asyncio.exceptions.TimeoutError:
             self._is_on = False
             self._is_available = False
@@ -104,6 +79,23 @@ class BasestationSwitch(SwitchEntity):
                 "Timeout occurred when trying to update basestation '%s'.",
                 self._mac,
             )
+        except Exception as e:
+            _LOGGER.error("Unexpected error updating basestation '%s': %s", self._mac, e)
+
+    async def _send_command(self, command):
+        """Send a command to the basestation."""
+        try:
+            async with BleakClient(self.get_ble_device(), timeout=30) as client:
+                await client.write_gatt_char(PWR_CHARACTERISTIC, command)
+                self._is_available = True
+        except asyncio.exceptions.TimeoutError:
+            self._is_available = False
+            _LOGGER.debug(
+                "Timeout occurred when sending command to basestation '%s'.",
+                self._mac,
+            )
+        except Exception as e:
+            _LOGGER.error("Unexpected error controlling basestation '%s': %s", self._mac, e)
 
     def get_ble_device(self):
         return bluetooth.async_ble_device_from_address(self._hass, str(self._mac))
